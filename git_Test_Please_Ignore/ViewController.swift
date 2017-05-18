88543b5c25efeb3c0084c279221a3080f42ec372
Marcus Gellel <Marcus1@Marcuss-MacBook-Pro.local>
2017-05-18 10:31:40 +1000

    Initial commit

---
+++ git_Test_Please_Ignore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_Test_Please_Ignore
+//
+//  Created by Marcus Gellel on 18/5/17.
+//  Copyright © 2017 Marcus Gellel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
